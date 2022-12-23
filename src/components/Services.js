46e44bcb69d35106b7e956daef94d3fc9ec0ca18
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-23 10:16:27 +0330

    Services in homepage

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,40 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <article className='header'>
+          <h3>
+            custom furniture <br />
+            built only for you
+          </h3>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos autem
+            ipsum laudantium facere unde nulla maiores corrupti enim voluptatum.
+            Sint modi quis itaque, doloremque quam enim dignissimos dolorem
+            officiis hic et commodi expedita iusto beatae temporibus nulla ea
+            esse magnam.
+          </p>
+        </article>
+
+        <div className='services-center'>
+          {services.map((service) => {
+            const { id, icon, title, text } = service
+
+            return (
+              <article key={id} className='service'>
+                <span className='icon'>{icon}</span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
